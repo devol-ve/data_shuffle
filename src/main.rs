9365b82d0c602eb60d0d54e6277f299b2986a363
Devon Lattery <devol.ve@outlook.com>
2024-03-09 23:16:22 -0600

    Added Scheduling
    Added schedule and cancel functions

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 * Author: Devon Lattery
 * Description: A program to consolidate and anonymize data in a directory.
 *              The program can be run once, every 30 seconds for a specified
-*              number of times, or scheduled to run daily, weekly, or monthly.
+*              number of times, or scheduled to run weekly.
 *******************************************************************************/
 
 use std::fs;
@@ -18,7 +18,7 @@ use rand::Rng;
 use std::thread::sleep;
 use console::Term;
 
-fn main() {    // TODO: Implement the main function
+fn main() {
     // Get the command line arguments
     let args: Vec<String> = env::args().collect();
     let data_dir = "data";
@@ -75,91 +75,157 @@ fn main() {    // TODO: Implement the main function
                 loop {
                     shuffle_data("data").unwrap();
                     count -= 1;
-                    if count == 0 {
+                    // If count is 0 or the user presses Esc, break the loop
+                    if term.read_key().unwrap() == console::Key::Escape || count == 0 {
                         break;
                     }
                     sleep(Duration::from_secs(30));
                 }
             }
-            "-S" | "--schedule" => { // TODO: Implement the schedule option
-                // Schedule the data shuffle to run daily, weekly, or monthly with the system's scheduler
+            "-S" | "--schedule" => {
+                // Schedule the data shuffle to run weekly with the system's scheduler
                 // Schedule the program to run weekly on Sunday at 12:00 AM if the user does not provide a time or day
                 let mut time = "00:00".to_string();
                 let mut day = "Sun".to_string();
                 if args.len() > 2 {
-                    match args[2].as_str() {
-                        "daily" => {
-                            // Schedule the data shuffle to run daily
-                            if args.len() > 3 {
-                                time = args[3].to_string();
+                    for mut i in 2..args.len() {
+                        match args[i].to_lowercase().as_str() {
+                            "su" | "sun" | "sunday" => {
+                                // Schedule for Sunday
+                                day = "Sun".to_string();
                             }
-                        }
-                        "weekly" => {
-                            // Schedule the data shuffle to run weekly
-                            if args.len() > 4 {
-                                day = args[3].to_string();
-                                time = args[4].to_string();
-                            } else if args.len() > 3 {
-                                day = args[3].to_string();
+                            "m" | "mon" | "monday" => {
+                                // Schedule for Monday
+                                day = "Mon".to_string();
                             }
-                        }
-                        "monthly" => {
-                            // Schedule the data shuffle to run monthly
-                            if args.len() > 4 {
-                                day = args[3].to_string();
-                                time = args[4].to_string();
-                            } else if args.len() > 3 {
-                                time = args[3].to_string();
+                            "t" | "tu" | "tue" | "tues" | "tuesday" => {
+                                // Schedule for Tuesday
+                                day = "Tue".to_string();
                             }
-                        }
-                        _ => {
-                            println!("Invalid schedule option");
-                            return;
+                            "w" | "wed" | "wednesday" => {
+                                // Schedule for Wednesday
+                                day = "Wed".to_string();
+                            }
+                            "h" | "th" | "thu" | "thurs" | "thursday" => {
+                                // Schedule for Thursday
+                                day = "Thu".to_string();
+                            }
+                            "f" | "fri" | "friday" => {
+                                // Schedule for Friday
+                                day = "Fri".to_string();
+                            }
+                            "s" | "sa" | "sat" | "saturday" => {
+                                // Schedule for Saturday
+                                day = "Sat".to_string();
+                            }
+                            "at" => {
+                                // Increment the index to get the time
+                                i += 1;
+
+                                // Validate the time
+                                if !is_valid_time(&args[i]) {
+                                    println!("Invalid time. Please provide a valid time in the format HH:MM.\nFor example, 13:30 for 1:30 PM.");
+                                    println!("Exiting...");
+                                    return;
+                                }
+                                // Schedule the data shuffle to run at a specific time
+                                if args.len() > i {
+                                    time = args[i].to_string();
+                                }
+                            }
+                            _ => {
+                                println!("Invalid schedule option");
+                                return;
+                            }
                         }
                     }
                 }
                 // Schedule the program to run weekly on Sunday at 12:00 AM if the user does not provide a time or day
                 println!("Scheduling the data shuffle to run {} on {} at {}...", args[2], day, time);
+                schedule(&day, &time);
             }
-            "-c" | "--cancel" => { // TODO: Implement the cancel option
-                // If the task is scheduled, cancel the scheduled data shuffle
-                
+            "-c" | "--cancel" => {
                 // Cancel the scheduled data shuffle
                 println!("Cancelling the scheduled data shuffle...");
+                cancel()
             }
             "-h" | "--help" => {
                 // Print the help message
-                println!("Usage: data_shuffler [OPTION] [ARGUMENT]");
+                println!("Usage: data_shuffler [OPTION]");
                 println!("Shuffle the data in the data directory");
                 println!("");
                 println!("Options:");
-                println!("  -l, --loop [COUNT]      Run shuffle_data() every 30 seconds");
-                println!("                          Repeat the number of times specified by the user or 100 times if the user does not provide a number");
-                println!("  -S, --schedule [OPTION] [ARGUMENT]");
-                println!("                          Schedule the data shuffle to run daily, weekly, or monthly with the system's scheduler");
-                println!("                          Schedule the program to run weekly on Sunday at 12:00 AM if the user does not provide a time or day");
-                println!("  -c, --cancel             Cancel the scheduled data shuffle");
-                println!("  -h, --help               Print the help message");
+                println!("  -l, --loop [COUNT]                 Run shuffle_data() every 30 seconds and repeat the number of times");
+                println!("                                     specified by the user or 100 times if the user does not provide a number");
+                println!("  -S, --schedule [DAY] [at TIME]");
+                println!("                                     Schedule the data shuffle to run every week with the system's scheduler");
+                println!("                                     Defaults to Sunday at 12:00 AM if the user does not provide a time or day");
+                println!("  -c, --cancel                       Cancel the scheduled data shuffle");
+                println!("  -h, --help                         Print the help message");
             }
             _ => {
                 println!("Invalid option");
             }
         }
     }
-//    // If the user provides the -l or --loop flag followed by a number
-//    //   Run shuffle_data() every 30 seconds
-//    //   Repeat the number of times specified by the user or indefinitely if the user does not provide a number
-//    // Else if the user provides the -S or --schedule flag followed by daily, weekly, or monthly
-//    //   Schedule the data shuffle to run daily, weekly, or monthly with the system's scheduler
-//    //   Schedule the program to run weekly on Sunday at 12:00 AM if the user does not provide a time or day
-//    // Else if the user provides the -c or --cancel flag
-//    //   Cancel the scheduled data shuffle
-//    // Else if the user provides the -h or --help flag
-//    //   Print the help message
-//    // Else
-//    //   Shuffle the data once
 }
 
+fn schedule(day: &str, time: &str) {
+    let cwd = env::current_dir().expect("Failed to get current directory").to_str().expect("Failed to convert path to string").to_string();
+    let exe = env::current_exe().expect("Failed to get current executable").to_str().expect("Failed to convert path to string").to_string();
+    let command = if cfg!(target_os = "windows") {
+        format!("schtasks /create /tn DataShuffler /tr \"cmd /c cd /d {} && {} --no-warning\" /sc weekly /d {} /st {}", cwd, exe, day, time)
+    } else {
+        let hrs = time.split(":").collect::<Vec<&str>>()[0];
+        let mins = time.split(":").collect::<Vec<&str>>()[1];
+        format!("( crontab -l; echo \"{} {} * * {} cd {} && {} --no-warning\"; ) | crontab -", mins, hrs, day, cwd, exe)
+    };
+    if cfg!(target_os = "windows") {
+        Command::new("powershell")
+            .args(&["-Command", &command])
+            .output()
+            .expect("Failed to execute command")
+    } else {
+        Command::new("sh")
+            .args(&["-c", &command])
+            .output()
+            .expect("Failed to execute command")
+    };
+}
+
+fn cancel() {
+    let exe = env::current_exe().expect("Failed to get current executable").to_str().expect("Failed to convert path to string").to_string();
+    let command = if cfg!(target_os = "windows") {
+        "schtasks /delete /tn DataShuffler /f".to_string()
+    } else {
+        format!("crontab -l | grep -v \" {} \" | crontab -", exe)
+    };
+    
+    if cfg!(target_os = "windows") {
+        Command::new("powershell")
+            .args(&["-Command", &command])
+            .output()
+            .expect("Failed to execute command")
+    } else {
+        Command::new("sh")
+            .args(&["-c", &command])
+            .output()
+            .expect("Failed to execute command")
+    };
+}
+
+fn is_valid_time(time: &str) -> bool {
+    let time = time.split(":").collect::<Vec<&str>>();
+    if time.len() != 2 {
+        return false;
+    }
+    let hour = time[0].parse::<u32>().unwrap();
+    let minute = time[1].parse::<u32>().unwrap();
+    if hour > 23 || minute > 59 {
+        return false;
+    }
+    true
+}
 
 fn is_admin() -> bool {
     let output = if cfg!(target_os = "windows") {
@@ -183,13 +249,20 @@ fn is_admin() -> bool {
 }
 
 fn shuffle_data(dir: &str) -> Result<(), Error> {
+    // Check if data directory exists
+    if !Path::new(dir).exists() {
+        return Err(Error::new(ErrorKind::Other, format!("Data directory {:?} does not exist", dir)));
+    }
+
     // Generate a random unix time within the last 10 days
     let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
     let ten_days = 10 * 24 * 60 * 60;
     let random_time = now - rand::thread_rng().gen_range(0..ten_days);
 
     // Set the system time to the generated unix time
-    change_time(random_time);
+    if is_admin() {
+        change_time(random_time);
+    }
 
     // For each directory in data directory
     for entry in fs::read_dir(dir)? {
@@ -206,7 +279,9 @@ fn shuffle_data(dir: &str) -> Result<(), Error> {
     }
 
     // Restore the system time to the current time
-    resync_time();
+    if is_admin() {
+        resync_time();
+    }
 
     Ok(())
 }
@@ -324,6 +399,15 @@ fn anonymize_data(dir: &str) -> Result<(), Error> {
     // Rename each file to a random number with a .csv extension
     for path in paths {
         println!("Anonymizing {}...", path.to_str().unwrap());
+
+        let path_extension = path.extension().unwrap().to_str().unwrap();
+        if path.is_dir() {
+            continue;
+        } else if path_extension != "csv" | path_extension != "txt" {
+            // Rename without changing the file extension
+            let new_name = format!("{}/{}.{}", dir, numbers.pop().unwrap(), path_extension);
+            continue;
+        }
         let new_name = format!("{}/{}.csv", dir, numbers.pop().unwrap());
         let mut file = fs::File::open(&path)?;
         let mut contents = String::new();
@@ -339,6 +423,28 @@ mod tests {
     use super::*;
     use std::fs;
     use std::path::Path;
+
+    #[test]
+    fn test_schedule() {
+        schedule("Sun", "00:00");
+    }
+
+    #[test]
+    fn test_cancel() {
+        cancel();
+    }
+
+    #[test]
+    fn test_is_valid_time() {
+        let result = is_valid_time("00:00");
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_is_invalid_time() {
+        let result = is_valid_time("24:00");
+        assert_eq!(result, false);
+    }
 
     #[test]
     fn test_is_admin() {
